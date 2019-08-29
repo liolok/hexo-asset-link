3a7273d29e40a911cff5f6708c7f03eb060f93f1
liolok <liolokit@gmail.com>
2019-08-29 16:11:45 +0800

    refactor main code and comments

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 'use strict';
 
 const url = require('url');
-let cheerio;
+const cheerio = require('cheerio');
 
-hexo.extend.filter.register('after_post_render', function(data) {
+hexo.extend.filter.register('after_post_render', function (data) {
   if (!hexo.config.post_asset_folder) return;
 
-  if (!cheerio) cheerio = require('cheerio');
-
-  // Get asset folder's path which is root-relative to site host
+  // Get asset folder's path which is root-relative to site host (strip suffix)
   const root = url.parse(data.permalink).pathname.replace(/\.[^/.]+$/, '/');
   console.info && console.info("Asset folder root-relative path: " + root);
 
   const keys = ['excerpt', 'more', 'content'];
   for (let i = 0; i < keys.length; i++) {
-    const $ = cheerio.load(data[keys[i]], {decodeEntities: false});
+    const $ = cheerio.load(data[keys[i]], { decodeEntities: false });
 
     function convertLink(element, attribute) {
-      $(element).each(function() {
-        const attr = $(this).attr(attribute);
-        if (!attr) return;  // no such attribute
-        if (url.parse(attr).protocol) return;  // absolute link
-        if (attr.startsWith('/')) return;  // root-relative link
-        if (attr.startsWith('#')) return;  // in-page fragment link
+      $(element).each(function () {
+        const link = $(this).attr(attribute);
+        if (!link  // no link found in element
+          || url.parse(link).protocal  // absolute link
+          || link.startsWith('/')  // root-relative link
+          || link.startsWith('#')  // in-page fragment link
+        ) return;  // only process base-relative link
 
-        // Get file's path which is base-relative to asset folder
-        const base = decodeURI(attr.replace(/.*?[^\.]\//, ''));
+        // Get file's path which is base-relative to asset folder (strip prefix)
+        const base = decodeURI(link.replace(/.*?[^\.]\//, ''));
         $(this).attr(attribute, root + base);
         console.info && console.info("Converted link: " + root + base);
       });
