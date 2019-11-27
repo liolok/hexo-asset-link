137860db06f918430446c3d30d8ce0173ffba6a9
liolok <i@liolok.com>
2019-11-27 15:21:48 +0800

    drop cheerio dependency with regex

--- index.js
+++ index.js
@@ -2,39 +2,24 @@
 
 const url = require('url');
 const chalk = require('chalk');
-const cheerio = require('cheerio');
 
-hexo.extend.filter.register('after_post_render', function (data) {
-  if (!hexo.config.post_asset_folder) return;
-
-  // Get asset folder's path which is root-relative to site host (strip suffix)
-  const root = url.parse(data.permalink).pathname.replace(/\.[^/.]+$/, '/');
-  hexo.log.info('Post asset folder: %s', chalk.blue(root));
-
-  const keys = ['content', 'excerpt', 'more'];
-  for (let i = 0; i < keys.length; i++) {
-    const $ = cheerio.load(data[keys[i]], { decodeEntities: false });
-
-    function convertLink(element, attribute) {
-      $(element).each(function () {
-        const link = $(this).attr(attribute);
-        if (!link  // no link found in element
-          || url.parse(link).protocol  // absolute link
-          || link.startsWith('/')  // root-relative link
-          || link.startsWith('#')  // in-page fragment link
-        ) return;  // only process base-relative link
-
-        // Get file's path which is base-relative to asset folder (strip prefix)
-        const base = decodeURI(link.replace(/^\.\//, '').replace(/^.*?\//, ''));
-        $(this).attr(attribute, root + base);
-        hexo.log.debug('Link converted in data[\'%s\']: %s',
-          keys[i], chalk.cyan(root + base));
-      });
-    }
-
-    convertLink('img', 'src');  // images
-    convertLink('a', 'href');  // other files
-
-    data[keys[i]] = $.html();
-  }
+hexo.extend.filter.register('before_post_render', function (data) {
+  // Need post asset folder option enabled and asset_dir attribute available
+  if (!hexo.config.post_asset_folder || !data.asset_dir) return;
+  // Make sure path separator is slash rather than back-slash.
+  let asset_dir = data.asset_dir.replace('\\', '/');
+  hexo.log.d('Post asset folder full path:', chalk.magenta(asset_dir));
+  // Split hierarchy, filter empty string, last one is asset folder's name.
+  let asset_dir_name = asset_dir.split('/').filter(i => i).pop();
+  hexo.log.d('Post asset folder name:', chalk.magenta(asset_dir_name));
+  // Start with './' or not, end with '/', this is how user write asset links in markdown.
+  let path_markdown = RegExp('(\.\/)?' + asset_dir_name + '\/', 'g');
+  if (!path_markdown.test(data.content)) return; // no asset link found, do nothing
+  // Permalink's pathname, supposed to start with '/'
+  let pathname = url.parse(data.permalink).pathname;
+  hexo.log.d('Post html path name:', chalk.magenta(pathname));
+  // Strip any suffix if exists, supposed to start and end with '/', this is where assets would be in html.
+  let path_html = pathname.replace(/\.[^/.]+$/, '/');
+  data.content = data.content.replace(path_markdown, path_html);
+  hexo.log.i('Path converted:', chalk.yellow(path_markdown.toString()), '→', chalk.green(path_html));
 });
