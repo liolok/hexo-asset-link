107a595c8a683accde4ba5eaa28a0b9e62237f09
Wataru Haniyama <haniyama.wataru+work@gmail.com>
2020-05-07 20:00:52 +0900

    don't convert link if it start http

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function convertLink(data) {
   let asset_dir_name = data.asset_dir.split(/[\/\\]/).filter(i => i).pop();
   hexo.log.d('Post asset folder name:', chalk.magenta(asset_dir_name));
   // Start with './' or not, end with '/', this is how user write asset links in markdown.
-  let path_markdown = RegExp('(\.\/)?' + asset_dir_name + '\/', 'g');
+  let path_markdown = RegExp('\\((?!http)(\\.\/)?' + asset_dir_name + '\/', 'g');
   if (!path_markdown.test(data.content)) return; // no asset link found, do nothing
   // Permalink's pathname, supposed to start with '/'
   let pathname = url.parse(data.permalink).pathname;
   hexo.log.d('Post html path name:', chalk.magenta(pathname));
   // Strip any suffix if exists, supposed to start and end with '/', this is where assets would be in html.
-  let path_html = pathname.replace(/\.[^/.]+$/, '/');
+  let path_html = '(' + pathname.replace(/\.[^/.]+$/, '/');
   data.content = data.content.replace(path_markdown, path_html);
   hexo.log.i('Path converted:', chalk.yellow(path_markdown.toString()), '→', chalk.green(path_html));
 }
